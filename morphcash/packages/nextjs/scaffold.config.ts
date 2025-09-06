bbfb808b747345966bb760a617dca417d3def290
Yaw Bempong <yawadubempong@gmail.com>
2025-09-06 10:39:54 +0000

    Chain error

--- morphcash/packages/nextjs/scaffold.config.ts
+++ morphcash/packages/nextjs/scaffold.config.ts
@@ -1,5 +1,4 @@
 import * as chains from "viem/chains";
-import { base, baseSepolia } from "./utils/customChains";
 
 export type BaseConfig = {
   targetNetworks: readonly chains.Chain[];
@@ -16,7 +15,7 @@ export const DEFAULT_ALCHEMY_API_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [base, baseSepolia],
+  targetNetworks: [chains.base, chains.baseSepolia],
   // The interval at which your front-end polls the RPC servers for new data (it has no effect if you only target the local network (default is 4000))
   pollingInterval: 30000,
   // This is ours Alchemy's default API key.
